6c69e94ebe019286f9d4b3ffe01b188a96a7b78d
Daniel Mills <ohshuckabees@gmail.com>
2013-02-25 21:42:15 -0500

    magic number removal

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 
  var size = 300;
+ var QR_MIN = 50;
+ var QR_MAX = 500;
 
  var getQRSize =function() {
 
     var tempsize = parseInt($('#inputsize').val());
-    if(!isNaN(tempsize) && tempsize < 50)
-    	size = 50;
-    else if(!isNaN(tempsize) && tempsize > 500)
-    	size = 500;
+    if(!isNaN(tempsize) && tempsize < QR_MIN)
+    	size = QR_MIN;
+    else if(!isNaN(tempsize) && tempsize > QR_MAX)
+    	size = QR_MAX;
     else if(isNaN(tempsize))
     	size = 300;
     else
@@ -37,30 +39,28 @@
             $('#qrresult').html(' <img src="'+qrcodetext+'" />').show();
 	});	
 
-     	$('#createvcard').bind('click',function() {
+    $('#createvcard').bind('click',function() {
 
-		var n = $('#inputn').val();
-		var a = $('#inputadd').val();
-		var p = $('#inputvphone').val();
-		var e = $('#inputvemail').val();
-		var u = $('#inputurl').val();
+  		var n = $('#inputn').val();
+  		var a = $('#inputadd').val();
+  		var p = $('#inputvphone').val();
+  		var e = $('#inputvemail').val();
+  		var u = $('#inputurl').val();
 
+  		var qrcodetext = createBaseLink() + "MECARD:N:"+n+";ADR:"+a+";TEL:"+p+";EMAIL:"+e+";URL:"+u+";;";
+  		$('#qrresult').html(' <img src="'+qrcodetext+'" />').show();
 
-		var qrcodetext = 
-		createBaseLink() + 
-		"MECARD:N:"+n+";ADR:"+a+";TEL:"+p+";EMAIL:"+e+";URL:"+u+";;";
+    });
 
-		$('#qrresult').html(' <img src="'+qrcodetext+'" />').show();
-      });
+    $('#createemail').bind('click',function() {
 
-      $('#createemail').bind('click',function() {
-		var address = $('#inputemail').val();
-		var subject = $('#inputsubject').val();
-		var body = $('#inputbody').val();
-		var qrcodetext = createBaseLink() +  'MATMSG:TO:' +address+ ';SUB:' + subject+ ';BODY:' +body+ ';';
-		$('#qrresult').html(' <img src="'+qrcodetext+'" />').show();
+    		var address = $('#inputemail').val();
+    		var subject = $('#inputsubject').val();
+    		var body = $('#inputbody').val();
+    		var qrcodetext = createBaseLink() +  'MATMSG:TO:' +address+ ';SUB:' + subject+ ';BODY:' +body+ ';';
+    		$('#qrresult').html(' <img src="'+qrcodetext+'" />').show();
 
-      });
+    });
 
       $('#createtweet').bind('click',function() {
       	var message = $('#inputtweet').val();
